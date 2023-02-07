79614072ee6ec689bbf29d54554eb9a4a3c9f98a
Prathamesh Kulkarni <varunkulkarni@gmail.com>
2023-02-06 20:16:33 -0500

    initialized project

---
+++ src/main/kotlin/com/zugzwang/services/consultingperformancemgmt/ConsultingPerformanceMgmtApplication.kt
@@ -0,0 +1,11 @@
+package com.zugzwang.services.consultingperformancemgmt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConsultingPerformanceMgmtApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConsultingPerformanceMgmtApplication>(*args)
+}
---
+++ src/test/kotlin/com/zugzwang/services/consultingperformancemgmt/ConsultingPerformanceMgmtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zugzwang.services.consultingperformancemgmt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConsultingPerformanceMgmtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
