90c78bf01c2c0f5b06b0bbee92742a2b9d6d15d4
Prathamesh Kulkarni <varunkulkarni@gmail.com>
2023-02-20 17:06:22 -0500

    start adding database support

--- src/main/kotlin/com/zugzwang/services/consultingperformancemgmt/model/Message.kt
+++ src/main/kotlin/com/zugzwang/services/consultingperformancemgmt/model/Message.kt
@@ -1,9 +1,17 @@
 package com.zugzwang.services.consultingperformancemgmt.model
 
+import jakarta.persistence.Column
+import jakarta.persistence.GeneratedValue
 import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
 import java.util.UUID
 
+@Table("messages")
 data class Message(
-    @Id val id: UUID,
+    @Id
+    @GeneratedValue
+    private val id: UUID,
+
+    @Column(name = "message_content")
     val msg: String
 )
--- src/test/kotlin/com/zugzwang/services/consultingperformancemgmt/controller/MessageControllerIntegrationTest.kt
+++ src/test/kotlin/com/zugzwang/services/consultingperformancemgmt/controller/MessageControllerIntegrationTest.kt
@@ -9,7 +9,7 @@ import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 
-private class MessageControllerTest : AbstractIntegrationTest() {
+private class MessageControllerIntegrationTest : AbstractIntegrationTest() {
 
     @Autowired // Use Spring DI to create MVC
     private lateinit var mockMvc: MockMvc // Enable hitting controller without actually making HTTP requests
