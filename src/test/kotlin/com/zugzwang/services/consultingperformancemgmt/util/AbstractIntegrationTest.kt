27619ab2e5513cdd1484fd76193e42ca568587ed
Prathamesh Kulkarni <varunkulkarni@gmail.com>
2023-03-27 17:32:56 -0400

    use profiles

--- src/test/kotlin/com/zugzwang/services/consultingperformancemgmt/util/AbstractIntegrationTest.kt
+++ src/test/kotlin/com/zugzwang/services/consultingperformancemgmt/util/AbstractIntegrationTest.kt
@@ -3,16 +3,16 @@ package com.zugzwang.services.consultingperformancemgmt.util
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
-import org.springframework.test.context.TestPropertySource
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @SpringBootTest // Perform integration tests with the service actually spun up
 @AutoConfigureMockMvc // Use Spring DI to instantiate underlying MVC web framework object
-@TestPropertySource(locations = ["classpath:application-test.properties"]) // Point to test overrides
+@ActiveProfiles("test") // Point to test properties
 @Testcontainers // Manages container lifecycle across tests
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD) // Indicate that each test requires resetting context to avoid side effects
 abstract class AbstractIntegrationTest {
