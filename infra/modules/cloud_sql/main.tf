a80a9622fa29b8517f41c511b4feb879c2bd4712
Prathamesh Kulkarni <varunkulkarni@gmail.com>
2023-03-28 23:32:22 -0400

    fix staging

--- infra/modules/cloud_sql/main.tf
+++ infra/modules/cloud_sql/main.tf
@@ -4,9 +4,10 @@ resource "random_id" "db_name_suffix" {
 
 resource "google_sql_database_instance" "db" {
   project = var.gcp_project_id
-  name = "postgres-instance-${random_id.db_name_suffix.hex}"
+  name = "postgres-${terraform.workspace}-${random_id.db_name_suffix.hex}"
   database_version = "POSTGRES_14"
   region = var.cloud_sql_region
+  deletion_protection = false
 
   # TODO: Figure out how to limit access to app and local dev
   # TODO: Add password validation policy
