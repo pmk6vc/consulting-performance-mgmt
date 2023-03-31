fbc0c59e69b5da0cbf337bacd2b4e465b81729cc
Prathamesh Kulkarni <varunkulkarni@gmail.com>
2023-03-31 09:13:00 -0400

    fix formatting

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -4,6 +4,6 @@ output "db_connection_string" {
 }
 
 output "artifact_registry_repo" {
-  value = module.artifact_registry.artifact_registry_repo_name
+  value       = module.artifact_registry.artifact_registry_repo_name
   description = "Artifact registry repo name"
 }
