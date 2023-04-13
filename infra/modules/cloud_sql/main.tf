ecbed41be451c3955fa7d3197341903d154254f8
Prathamesh (Varun) Kulkarni <varunkulkarni@gmail.com>
2023-04-13 17:40:38 -0400

    delete db and disable cloud run deploy (#26)
    * delete db and disable cloud run deploy
    
    * remove db output
    
    * comment out all db refs
    
    * only comment out main cloud sql

--- infra/modules/cloud_sql/main.tf
+++ infra/modules/cloud_sql/main.tf
@@ -2,30 +2,30 @@ resource "random_id" "db_name_suffix" {
   byte_length = 4
 }
 
-resource "google_sql_database_instance" "db" {
-  project = var.gcp_project_id
-  name = "postgres-${terraform.workspace}-${random_id.db_name_suffix.hex}"
-  database_version = "POSTGRES_14"
-  region = var.cloud_sql_region
-  deletion_protection = false
+#resource "google_sql_database_instance" "db" {
+#  project = var.gcp_project_id
+#  name = "postgres-${terraform.workspace}-${random_id.db_name_suffix.hex}"
+#  database_version = "POSTGRES_14"
+#  region = var.cloud_sql_region
+#  deletion_protection = false
+#
+#  # TODO: Figure out how to limit access to app and local dev
+#  # TODO: Add password validation policy
+#  settings {
+#    tier = "db-f1-micro"
+#    activation_policy = "ALWAYS"
+#    disk_size = 20
+#    disk_autoresize = true
+#    disk_autoresize_limit = 25
+#    database_flags {
+#      name  = "max_connections"
+#      value = "50"
+#    }
+#  }
+#}
 
-  # TODO: Figure out how to limit access to app and local dev
-  # TODO: Add password validation policy
-  settings {
-    tier = "db-f1-micro"
-    activation_policy = "ALWAYS"
-    disk_size = 20
-    disk_autoresize = true
-    disk_autoresize_limit = 25
-    database_flags {
-      name  = "max_connections"
-      value = "50"
-    }
-  }
-}
-
-resource "google_sql_user" "db_user" {
-  instance = google_sql_database_instance.db.name
-  name = var.db_username
-  password = var.db_password
-}
+#resource "google_sql_user" "db_user" {
+#  instance = google_sql_database_instance.db.name
+#  name = var.db_username
+#  password = var.db_password
+#}
