beadfa2162d0dcf90f615bfe2872822cc1eef5ea
Prathamesh Kulkarni <varunkulkarni@gmail.com>
2023-04-13 17:33:47 -0400

    remove db output

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,7 +1,7 @@
-output "db_connection_string" {
-  value       = module.cloud_sql.db_connection_name
-  description = "Cloud SQL DB connection name"
-}
+#output "db_connection_string" {
+#  value       = module.cloud_sql.db_connection_name
+#  description = "Cloud SQL DB connection name"
+#}
 
 output "artifact_registry_repo" {
   value       = module.artifact_registry.artifact_registry_repo_name
